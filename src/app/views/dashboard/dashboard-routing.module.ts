b41fad960dff415639f2e9e66a75ed7b5a0c97ac
zimejin <moonspawn@live.co.uk>
2019-01-25 07:37:24 +0100

    customer dashboard ui created

--- src/app/views/dashboard/dashboard-routing.module.ts
+++ src/app/views/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboadDefaultComponent } from './dashboad-default/dashboad-default.component';
-import { DashboardV2Component } from './dashboard-v2/dashboard-v2.component';
-import { AtmDispenseErrorComponent } from './dashboad-default/atm-dispense-error/atm-dispense-error.component';
-import { CardIssueComponent } from './dashboad-default/card-issue/card-issue.component';
-import { EChannelsErrorComponent } from './dashboad-default/e-channels-error/e-channels-error.component';
-import { ServiceIssueComponent } from './dashboad-default/service-issue/service-issue.component';
+import { DashboadDefaultComponent } from './dashboad-customer-complaints/dashboad-default.component';
+import { DashboardV2Component } from './dashboard-customer-issue-tracking/dashboard-v2.component';
+import { AtmDispenseErrorComponent } from './dashboad-customer-complaints/atm-dispense-error/atm-dispense-error.component';
+import { CardIssueComponent } from './dashboad-customer-complaints/card-issue/card-issue.component';
+import { EChannelsErrorComponent } from './dashboad-customer-complaints/e-channels-error/e-channels-error.component';
+import { ServiceIssueComponent } from './dashboad-customer-complaints/service-issue/service-issue.component';
 
 const routes: Routes = [
   {
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboadDefaultComponent } from './dashboad-default/dashboad-default.component';
+import { DashboadDefaultComponent } from './dashboad-customer-complaints/dashboad-default.component';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { SharedComponentsModule } from 'src/app/shared/components/shared-components.module';
-import { DashboardV2Component } from './dashboard-v2/dashboard-v2.component';
+import { DashboardV2Component } from './dashboard-customer-issue-tracking/dashboard-v2.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AtmDispenseErrorComponent } from './dashboad-default/atm-dispense-error/atm-dispense-error.component';
-import { CardIssueComponent } from './dashboad-default/card-issue/card-issue.component';
-import { EChannelsErrorComponent } from './dashboad-default/e-channels-error/e-channels-error.component';
-import { ServiceIssueComponent } from './dashboad-default/service-issue/service-issue.component';
+import { AtmDispenseErrorComponent } from './dashboad-customer-complaints/atm-dispense-error/atm-dispense-error.component';
+import { CardIssueComponent } from './dashboad-customer-complaints/card-issue/card-issue.component';
+import { EChannelsErrorComponent } from './dashboad-customer-complaints/e-channels-error/e-channels-error.component';
+import { ServiceIssueComponent } from './dashboad-customer-complaints/service-issue/service-issue.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomFormsModule } from 'ng2-validation';
 @NgModule({
